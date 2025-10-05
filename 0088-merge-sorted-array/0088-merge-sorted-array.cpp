0f13ac7ce5b47e28f33ae5e14cfd2fd92fd9d49d
Ansh-ext <anshdeep9926@gmail.com>
2025-10-06 01:41:32 +0530

    0088-merge-sorted-array.cpp

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
           int last = m + n - 1;
 
-        //merge in reverse order
+       
         while(m > 0 && n > 0){
             if(nums1[m-1] > nums2[n-1]){
                 nums1[last] = nums1[m-1];
@@ -15,7 +15,7 @@ public:
             last--;
         }
 
-        //fill nums1 with leftover of nums2
+       
         while(n>0){
             nums1[last] = nums2[n-1];
             last--;
